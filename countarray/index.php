94d5a1bca6a2efa66df7c0a3f201743789240246
ritakumari93 <62473242+ritakumari93@users.noreply.github.com>
2021-05-07 00:05:13 +0530

    Create index.php

---
+++ countarray/index.php
@@ -0,0 +1,24 @@
+<?php 
+function getPairsCount($arr, $n, $sum)
+{
+    
+    $count = 0;
+ 
+    for ($i = 0; $i < $n; $i++)
+        for ($j = $i + 1; $j < $n; $j++)
+            if ($arr[$i] + $arr[$j] == $sum)
+                $count++;
+ 
+    return $count;
+}
+ 
+    
+    $arr = array(1, 5, 7, -1, 5) ;
+    $arr=array_unique($arr);
+    $n = sizeof($arr);
+    $sum = 6;
+    echo "Count of pairs is "
+         , getPairsCount($arr, $n, $sum);
+          
+
+?>
